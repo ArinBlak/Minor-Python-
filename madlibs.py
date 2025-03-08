4ad87dc106aa36d3f0d47647d4dfda66d25ad70d
ArinBlak <arindamray.22@nshm.edu.in>
2025-03-08 11:43:03 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+#A game of Madlibs, which is a word game
+adjective1 = input("Enter an adjective (description)")
+noun1 = input("Enter a noun (thing, place, animal etc)")
+adjective2 = input("Enter an adjective (description)")
+verb1 = input("Enter a verb ")
+adjective3 = input("Enter an adjective (description)")
+
+print(f"Today I went to a {adjective1} zoo")
+print(f"The zoo had a {noun1}")
+print(f"The {noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}")
